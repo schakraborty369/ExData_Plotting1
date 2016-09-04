590417044c05367a26ef9a28c9eb7092f4f07cd4
Soumitra <schakraborty369@gmail.com>
2016-09-04 21:02:37 +0530

    Final commit with minor garnishing

--- Plot1.R
+++ Plot1.R
@@ -1,10 +1,12 @@
 library(lubridate)
-rm(list=ls())
+rm(list=ls()) # removed the earlier objects from environment
 data <- read.csv2("household_power_consumption.txt")
 data$Date <- dmy(data$Date)
 data$Time <- hms(data$Time)
+
 f_data <- subset(data, year(Date) == 2007 & month(Date) == 02 & (day(Date) == 1 | day(Date) == 2))
 Numeric_var <- as.numeric(levels(f_data$Global_active_power))[f_data$Global_active_power]
+
 png(filename = "Plot1.png", height = 480, width = 480, bg = "transparent")
 hist(Numeric_var, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", 
      main = "Global Active Power", col = "red")
--- Plot2.R
+++ Plot2.R
@@ -3,6 +3,8 @@ data <- read.csv2("household_power_consumption.txt")
 f_data <- filter(data, Date %in% c("1/2/2007","2/2/2007")) 
 f_data$DateTime <- strptime(paste(f_data$Date,f_data$Time),"%d/%m/%Y %H:%M:%S")
 
+# converted factor variablees into numeric
+
 for (i in 3:9) {f_data[,i] <- as.numeric(levels(f_data[,i]))[f_data[,i]]}
 
 png("plot2.png", height = 480, width = 480, bg = "transparent")
